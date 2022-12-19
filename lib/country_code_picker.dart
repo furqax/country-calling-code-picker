72a2adcb725b27634302985d36a8e252a4801ad5
furqax <98593270+furqax@users.noreply.github.com>
2022-12-19 08:14:38 -0800

    Update country_code_picker.dart

--- lib/country_code_picker.dart
+++ lib/country_code_picker.dart
@@ -8,7 +8,7 @@ import 'functions.dart';
 
 const TextStyle _defaultItemTextStyle = const TextStyle(fontSize: 16);
 const TextStyle _defaultSearchInputStyle = const TextStyle(fontSize: 16);
-const String _kDefaultSearchHintText = 'Search country name, code';
+const String _kDefaultSearchHintText = 'Search country/Region name, code';
 const String countryCodePackageName = 'country_calling_code_picker';
 
 class CountryPickerWidget extends StatefulWidget {
